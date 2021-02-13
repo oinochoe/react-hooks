c458f5d32851b7bc685f3aecb48e3ce99bf61022
oinochoe <oinochoe@naver.com>
2021-02-13 21:12:45 +0900

    useAsync hook 만들기

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import useAsync from './useAsync';
+import User from './User';
 
 async function getUsers() {
     const response = await axios.get('https://jsonplaceholder.typicode.com/users');
@@ -9,6 +10,7 @@ async function getUsers() {
 
 function Users() {
     const [state, refetch] = useAsync(getUsers, [], true);
+    const [userId, setUserId] = useState(null);
 
     const { loading, data: users, error } = state;
     if (loading) return <div>로딩중...</div>;
@@ -19,7 +21,7 @@ function Users() {
         <>
             <ul>
                 {users.map((user) => (
-                    <li key={user.id}>
+                    <li key={user.id} onClick={() => setUserId(user.id)}>
                         {user.username} ({user.name})
                     </li>
                 ))}
@@ -27,6 +29,7 @@ function Users() {
             <button type='button' onClick={refetch}>
                 다시 불러오기
             </button>
+            { userId && <User id={userId} />}
         </>
     );
 }

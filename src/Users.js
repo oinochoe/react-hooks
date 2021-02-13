be94e81ce8bb186f265bbcf07bcace5807c04ba3
oinochoe <oinochoe@naver.com>
2021-02-13 16:29:47 +0900

    useeffect로 구현하기

--- src/Users.js
+++ src/Users.js
@@ -6,38 +6,41 @@ function Users() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchUsers = async() => {
-            try {
-                setUsers(null);
-                setError(null);
-                setLoading(true);
+    const fetchUsers = async () => {
+        try {
+            setUsers(null);
+            setError(null);
+            setLoading(true);
 
-                const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-                setUsers(response.data);
-
-            } catch (e) {
-                setError(e);
-            }
-
-            setLoading(false);
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            setUsers(response.data);
+        } catch (e) {
+            setError(e);
         }
+
+        setLoading(false);
+    };
+
+    useEffect(() => {
         fetchUsers();
     }, []);
 
-    if(loading) return <div>로딩중...</div>;
-    if(error) return <div>에러발생...</div>;
-    if(!users) return null;
+    if (loading) return <div>로딩중...</div>;
+    if (error) return <div>에러발생...</div>;
+    if (!users) return null;
 
-    return <ul>
-        {
-            users.map(user =>
-                <li key={user.id}>
-                    {user.username} ({user.name})
-                </li>
-            )
-        }
-    </ul>
+    return (
+        <>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        {user.username} ({user.name})
+                    </li>
+                ))}
+            </ul>
+            <button type="button" onClick={fetchUsers}>다시 불러오기</button>
+        </>
+    );
 }
 
 export default Users;

3defb2f719c715c40e2b88c37f5dfc8291215a47
oinochoe <oinochoe@naver.com>
2021-02-13 21:53:41 +0900

    react-async 사용

--- src/User.js
+++ src/User.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 
-async function getUser(id) {
+async function getUser({ id }) {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
     return response.data;
 }
 
 const User = ({ id }) => {
-    const [state] = useAsync(() => getUser(id), [id]);
-    const { loading, data: user, error } = state;
+    const { data: user, error ,isLoading } = useAsync({
+        promiseFn: getUser,
+        id,
+        watch: id,
+    });
 
-    if(loading) return <div>로딩중..</div>
+    if (isLoading) return <div>로딩중..</div>;
     if(error) return <div>에러 발생..</div>
     if(!user) return null;
 
